bfc441253bfcd1780001ce9a47cb68fee0ea6dbb
Arthur Frade de Araújo <arthur.frade@loft.com.br>
2022-09-14 00:49:32 -0300

    kafka server publishing on is-even response producer

--- server/server.go
+++ server/server.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+	startServer()
+}
+
+func startServer() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost",
 		"group.id":          "server",
 	})
-	defer c.Close()
+	defer consumer.Close()
 
 	if err != nil {
 		fmt.Printf("Failed to create consumer: %s", err)
@@ -21,13 +26,39 @@ func main() {
 
 	fmt.Println("Waiting for messages...")
 
-	c.SubscribeTopics([]string{"is-even-request"}, nil)
+	isEvenProducer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+
+	defer isEvenProducer.Close()
+
+	consumer.SubscribeTopics([]string{"is-even-request"}, nil)
 	for {
-		message, err := c.ReadMessage(-1) // indefinite wait
+		message, err := consumer.ReadMessage(-1) // indefinite wait
 		if err != nil {
 			continue
 		}
-		fmt.Printf("Consumed message from topic %s: value = %s\n",
-			*message.TopicPartition.Topic, string(message.Value))
+		isEvenMessage(isEvenProducer, string(message.Value))
 	}
+}
+
+func isEvenMessage(producer *kafka.Producer, message string) error {
+	integer, err := strconv.Atoi(message)
+	fmt.Printf("Consumed message from topic is-even-request: value = %d\n", integer)
+	if err != nil {
+		return err
+	}
+	var response string
+	if integer%2 == 0 {
+		response = "yes"
+	} else {
+		response = "no"
+	}
+	responseTopic := "is-even-reply"
+	producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &responseTopic, Partition: kafka.PartitionAny},
+		Value:          []byte(response),
+	}, nil)
+	return nil
 }

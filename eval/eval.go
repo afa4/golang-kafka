a6a07c797ea84a3bf3cca0a370f44324ece9fa1c
Arthur Frade de Araújo <arthur.frade@loft.com.br>
2022-09-14 06:48:04 -0300

    fix eval code

--- eval/eval.go
+++ eval/eval.go
@@ -5,13 +5,26 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
-	createClientProcess(10)
+	//numberOfClientsArray := []int{1, 5, 10, 20, 40, 80}
+	numberOfClientsArray := []int{1, 5, 10, 20}
+	requestsPerClient := 1000
+	var wg sync.WaitGroup
+	for _, numberOfClients := range numberOfClientsArray {
+		fmt.Printf("For %d client(s)\n", numberOfClients)
+		for i := 0; i < numberOfClients; i++ {
+			wg.Add(1)
+			go createClientProcess(requestsPerClient, &wg)
+		}
+		wg.Wait()
+	}
 }
 
-func createClientProcess(numberOfRequests int) {
+func createClientProcess(numberOfRequests int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	invokeClientCommand := fmt.Sprintf("go run client/client.go %d", numberOfRequests)
 	bash := exec.Command("bash", "-c", invokeClientCommand)
 	output, err := bash.Output()
@@ -22,6 +35,7 @@ func createClientProcess(numberOfRequests int) {
 	}
 
 	outputStr := string(output)
+	//fmt.Println(outputStr)
 	repliesDurations := strings.Split(outputStr, "\n")
 	numberOfDurations := len(repliesDurations) - 1
 	totalDuration := 0
@@ -33,5 +47,4 @@ func createClientProcess(numberOfRequests int) {
 		totalDuration += replyDuration
 	}
 	fmt.Printf("mean %d\n", totalDuration/numberOfDurations)
-
 }
